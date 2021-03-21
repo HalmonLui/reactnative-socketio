d74003f522fe1d12fcfdd7698b7b748c4f93e6cb
Lui <luih@wit.edu>
2021-03-21 14:59:41 -0400

    working on press and release socket logic

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -5,13 +5,14 @@ import { StyleSheet, Text, View, TextInput, NativeModules, TouchableOpacity } fr
 
 const SharedStorage = NativeModules.SharedStorage;
 // Need to test with ngrok with mobile phone
-const socketURL = 'http://d1d6093f3136.ngrok.io'
+const socketURL = 'http://56c128b62deb.ngrok.io'
 const socket = io(socketURL)
 
 export default function App() {
   const [text, onChangeText] = React.useState("");
   const [chatMessage, setChatMessage] = React.useState('testttt')
   const [chatMessages, onChangeChatMessages] = React.useState([])
+  const [buttonPressed, onIsButtonPressed] = React.useState('123abc')
 
 
   // Same as componentDidMount()
@@ -19,19 +20,19 @@ export default function App() {
     console.log('joining room')
     socket.on("chat message", msg => {
       console.log('use effect socket on testt', msg)
-      console.log('the messages before', chatMessages)
-      updateChatMessages(msg)
-      console.log('the messages after', chatMessages)
+      onChangeChatMessages(chatMessages => ([...chatMessages, msg]))
     })
-
+    socket.on("button press", pressed => {
+      console.log('button has been pressed')
+      onIsButtonPressed('lalala')
+    })
+    socket.on("button release", released => {
+      console.log('button has been released')
+      onIsButtonPressed('hihihi')
+    })
     // Clean up effect
     return () => socket.disconnect();
   }, []);
-
-  const updateChatMessages = (msg) => {
-    onChangeChatMessages(chatMessages => ([...chatMessages, msg]))
-    console.log('the chat message', chatMessage)
-  }
 
   const chatMessagesElement = chatMessages.map(chatMessage => (
       <Text key={chatMessage} style={{borderWidth: 2, height: 20, top: 5}}>{chatMessage}</Text>
@@ -42,6 +43,16 @@ export default function App() {
     console.log('emitted socket thing')
     setChatMessage('')
     console.log('messages', chatMessages)
+  }
+
+  const pressButton = () => {
+    socket.emit('button press', true)
+    console.log('emitted button press')
+  }
+
+  const releaseButton = () => {
+    socket.emit('button release', true)
+    console.log('emitted button release')
   }
 
   return (
@@ -60,6 +71,13 @@ export default function App() {
             setChatMessage(msg);
           }}
         />
+      <Text>Button Pressed: {buttonPressed}</Text>
+      <TouchableOpacity
+        onPressIn={pressButton}
+        onPressOut={releaseButton}
+      >
+        <Text>Button</Text>
+      </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
